3970de501b6e5944bcf21cb40b536ab8c663a454
Zenfection <kietgolx65234@gmail.com>
2022-11-15 00:36:56 +0700

    update

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,13 +1,1 @@
-import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
-import 'package:nb_utils/nb_utils.dart';
 
-import 'package:shop_order/screens/GSLoginScreen.dart';
-import 'package:shop_order/screens/GSRegisterScreen.dart';
-import 'package:shop_order/utils/GSColors.dart';
-import 'package:shop_order/utils/GSImages.dart';
-import 'package:shop_order/main/utils/AppColors.dart';
-
-Future<> main() async {
-  
-}
